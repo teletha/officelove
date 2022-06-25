100dcce891a3857dda106cc513ad3b83540bd99d
Teletha <Teletha.T@gmail.com>
2022-06-25 20:04:21 +0900

    rename package

--- src/test/java/offishell/word/AssertionHelper.java
+++ src/test/java/offishell/word/AssertionHelper.java
@@ -7,7 +7,7 @@
  *
  *          https://opensource.org/licenses/MIT
  */
-package offishell.msoffice;
+package offishell.word;
 
 import java.util.Deque;
 import java.util.LinkedList;
--- src/test/java/offishell/word/WordParagraphTest.java
+++ src/test/java/offishell/word/WordParagraphTest.java
@@ -7,7 +7,7 @@
  *
  *          https://opensource.org/licenses/MIT
  */
-package offishell.msoffice;
+package offishell.word;
 
 import java.io.IOException;
 
