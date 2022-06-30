7ea791c7b3018a78e53bf6ef2d64d912abcc3b7c
teletha <teletha@users.noreply.github.com>
2022-06-30 06:10:53 +0000

    deploy: 857025d97f5c90cba8d1acba737409f7e1aa6582

---
+++ root.js
@@ -0,0 +1,103 @@
+const root = {
+	"docs": [],
+	"modules": [],
+	"packages": [
+		"officelove",
+		"officelove.excel",
+		"officelove.expression",
+		"officelove.word"
+	],
+	"types": [
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "ConstantVariable",
+			"packageName": "officelove.expression",
+			"type": "Interface"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Excel",
+			"packageName": "officelove.excel",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Excel.Row",
+			"packageName": "officelove.excel",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "ExpressionResolver",
+			"packageName": "officelove.expression",
+			"type": "Interface"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "LibreOffice",
+			"packageName": "officelove",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "PhoneticAware",
+			"packageName": "officelove.excel",
+			"type": "Functional"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Variable",
+			"packageName": "officelove.expression",
+			"type": "Interface"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "VariableContext",
+			"packageName": "officelove.expression",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Word",
+			"packageName": "officelove.word",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Word.Section",
+			"packageName": "officelove.word",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "WordCellStyle",
+			"packageName": "officelove.word",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "WordHeleper",
+			"packageName": "officelove.word",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "WordStyle",
+			"packageName": "officelove.word",
+			"type": "Class"
+		}
+	]
+}
