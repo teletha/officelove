1202b31ae86af81b883d7b569b67a31031312dbf
teletha <teletha@users.noreply.github.com>
2023-07-23 04:18:24 +0000

    deploy: c23b2843f747c5a295a55457e24ef09db5220664

--- root.js
+++ root.js
@@ -18,13 +18,6 @@ const root = {
 		{
 			"modifiers": {
 				"#": "java.util.Collections$UnmodifiableSet",},
-			"name": "DocExpression",
-			"packageName": "officelove.word",
-			"type": "Class"
-		},
-		{
-			"modifiers": {
-				"#": "java.util.Collections$UnmodifiableSet",},
 			"name": "Excel",
 			"packageName": "officelove.excel",
 			"type": "Class"
@@ -77,13 +70,6 @@ const root = {
 			"name": "PhoneticAware",
 			"packageName": "officelove.excel",
 			"type": "Functional"
-		},
-		{
-			"modifiers": {
-				"#": "java.util.Collections$UnmodifiableSet",},
-			"name": "TemplateDoc",
-			"packageName": "officelove.word",
-			"type": "Class"
 		},
 		{
 			"modifiers": {
