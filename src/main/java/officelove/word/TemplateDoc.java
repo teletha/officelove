4a89bade0eb77a8787d8eb3a8ceda6d3102376f2
Teletha <Teletha.T@gmail.com>
2023-07-31 23:15:15 +0900

    update license

--- src/main/java/officelove/word/TemplateDoc.java
+++ src/main/java/officelove/word/TemplateDoc.java
@@ -1,10 +1,11 @@
 /*
- * Copyright (C) 2023 Nameless Production Committee
+ * Copyright (C) 2023 The OFFICELOVE Development Team
  *
  * Licensed under the MIT License (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
+ *          https://opensource.org/licenses/MIT
  */
 package officelove.word;
 
