2e8ea8d4451ff7081d569e564c5ef49a6dd0866a
Teletha <Teletha.T@gmail.com>
2025-04-22 05:59:09 +0900

    fix header

--- src/project/java/Project.java
+++ src/project/java/Project.java
@@ -8,16 +8,6 @@
  *
  *          https://opensource.org/licenses/MIT
  */
-
-/*
- * Copyright (C) 2023 The OFFICELOVE Development Team
- *
- * Licensed under the MIT License (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *          https://opensource.org/licenses/MIT
- */
 public class Project extends bee.api.Project {
 
     {
